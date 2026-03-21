3ba8714697b2763143691287f72803d2c287f5d6
pedro <pedro.a.vieira21@gmail.com>
2026-03-21 17:48:56 -0300

    Inclusao de interface

--- tp3/src/main/java/org/example/banco/service/ContaService.java
+++ tp3/src/main/java/org/example/banco/service/ContaService.java
@@ -1,54 +1,23 @@
 package org.example.banco.service;
 
-import lombok.RequiredArgsConstructor;
-import org.example.banco.exceptions.ContaExeption;
-import org.example.banco.repository.ContaRepository;
-import org.springframework.stereotype.Service;
 import org.example.banco.entity.Conta;
-import org.springframework.transaction.annotation.Transactional;
+import org.example.banco.exceptions.ContaExeption;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
-public class ContaService {
-    private final ContaRepository contaRepository;
+public interface ContaService {
+    void excluirContaDb(Long id);
+    List<Conta> consultarContasDb();
 
-    @Transactional
-    public void excluirContaDb(Long id) {
-        contaRepository.deleteById(id);
-    }
+    Conta consultarContaDb(Long id) throws ContaExeption ;
+    void incluirContaDb(String nome, BigDecimal saldo) ;
 
-    public List<Conta> consultarContasDb() {
-        return contaRepository.findAll();
-    }
+    void depositar(long l, BigDecimal v) throws ContaExeption ;
 
-    public Conta consultarContaDb(Long id) throws ContaExeption {
-        return contaRepository.findById(id).orElseThrow(() -> new ContaExeption("Conta não existe"));
-    }
-    @Transactional
-    public void incluirContaDb(String nome, BigDecimal saldo) {
-        Conta conta = new Conta(null, nome, (saldo));
-        contaRepository.save(conta);
-    }
+    void debitar(long l, BigDecimal v) throws ContaExeption ;
 
-    @Transactional
-    public void depositar(long l, BigDecimal v) throws ContaExeption {
-        Conta c = consultarContaDb(l);
-        c.depositar((v));
-    }
-    @Transactional
-    public void debitar(long l, BigDecimal v) throws ContaExeption {
-        Conta c = consultarContaDb(l);
-        c.debitar((v));
-        contaRepository.save(c);
-    }
-
-    @Transactional
-    public void transferir(long origem, long destino, BigDecimal v) throws ContaExeption {
-        debitar(origem, v);
-        depositar(destino, v);
-
-    }
+    void transferir(long origem, long destino, BigDecimal v) throws ContaExeption ;
 }
